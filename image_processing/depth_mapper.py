d6673e6e0a6b3ab2a712b97ace26d9f65115b854
rtmccune <rmccune@ncsu.edu>
2025-02-26 16:09:19 -0500

    Corrected bug in calc_depths method for images without ponds. Processed results for all CB_03 imagery.

--- image_processing/depth_mapper.py
+++ image_processing/depth_mapper.py
@@ -15,56 +15,31 @@ class DepthMapper:
 
     def label_ponds(self, gpu_label_array):
         labels_squeezed = gpu_label_array.squeeze()
-        mask = (labels_squeezed == 1)
-        masked_elevations = cp.where(mask, self.elev_grid, cp.nan)
+        mask = (labels_squeezed == 1)  # Boolean mask
 
-        masked_labels = cp.where(gpu_label_array == 1, gpu_label_array, 0)
-        masked_labels = cp.squeeze(masked_labels)
+        # Create binary mask directly as uint8 (avoids redundant cp.where)
+        masked_labels = mask.astype(cp.uint8)
 
-        closed_data = binary_closing(masked_labels, structure=cp.ones((3, 3)))
+        # Apply binary closing (morphological operation)
+        closed_data = binary_closing(masked_labels, structure=cp.ones((3, 3), dtype=cp.uint8))
+
+        # Label connected components
         labeled_data, num_features = label(closed_data)
 
-        min_size = 500  # Minimum size of a pond
+        # Remove small ponds
+        min_size = 100
         unique, counts = cp.unique(labeled_data, return_counts=True)
-        small_ponds = unique[counts < min_size]  # Identify small ponds
-        labeled_data[cp.isin(labeled_data, small_ponds)] = 0
+        small_ponds = unique[counts < min_size]
 
-        # Reapply the label function to relabel the remaining ponds
+        # In-place update (avoid unnecessary memory allocation)
+        mask_small_ponds = cp.isin(labeled_data, small_ponds)
+        labeled_data[mask_small_ponds] = 0
+
+        # Relabel remaining ponds
         labeled_data, num_features = label(labeled_data)
+
         return labeled_data
 
-    # def extract_contours(self, labeled_data, gpu_label_array):
-    #     unique_ponds = cp.unique(labeled_data)
-    #     unique_ponds = unique_ponds[unique_ponds != 0]  # Exclude background label
-
-    #     pond_contours = {}
-    #     arr = cp.where(gpu_label_array.squeeze() == 1, self.elev_grid, 0)
-
-    #     for pond_id in unique_ponds:
-    #         pond_mask = (cp.asnumpy(labeled_data) == pond_id.item())
-    #         pond_arr = np.where(pond_mask, cp.asnumpy(arr), 0)
-
-    #         contours = find_contours(pond_arr, level=0.5)
-    #         pond_contours[pond_id.item()] = contours
-
-    #     contour_pixels_per_pond = {}
-    #     contour_values_per_pond = {}
-
-    #     for pond_id, contours in pond_contours.items():
-    #         contour_pixels = []
-    #         contour_values = []
-
-    #         for contour in contours:
-    #             for point in contour:
-    #                 y, x = np.round(point).astype(int)
-    #                 if 0 <= x < cp.asnumpy(arr).shape[1] and 0 <= y < cp.asnumpy(arr).shape[0]:
-    #                     contour_pixels.append((x, y))
-    #                     contour_values.append(cp.asnumpy(arr)[y, x])
-
-    #         contour_pixels_per_pond[pond_id] = np.array(contour_pixels)
-    #         contour_values_per_pond[pond_id] = np.array(contour_values)
-
-    #     return contour_pixels_per_pond, contour_values_per_pond
     def extract_contours(self, labeled_data, gpu_label_array):
         unique_ponds = cp.unique(labeled_data)
         unique_ponds = unique_ponds[unique_ponds != 0]  # Exclude background label
@@ -107,11 +82,16 @@ class DepthMapper:
 
         return contour_pixels_per_pond, contour_values_per_pond
 
-
     def calculate_depths(self, labeled_data, contour_values_per_pond):
         pond_depths = {}
 
-        for pond_id in cp.unique(labeled_data):
+        unique_pond_ids = cp.unique(labeled_data)
+        
+        if len(unique_pond_ids) == 1 and unique_pond_ids[0] == 0:
+            pond_depths[1] = cp.full_like(self.elev_grid, cp.nan)
+            return pond_depths
+        
+        for pond_id in unique_pond_ids:
             if pond_id == 0:  # Skip background
                 continue
 
@@ -128,6 +108,7 @@ class DepthMapper:
         return pond_depths
 
     def combine_depth_maps(self, pond_depths):
+
         combined_depth_map = pond_depths[1]
 
         for i in range(2, len(pond_depths) + 1):
@@ -173,6 +154,7 @@ class DepthMapper:
             store = zarr.open_group(depth_map_zarr_dir, mode='a')
             
             image_name = row['image_name']
+            
             depth_map = row['depth_map']
 
             store[image_name] = depth_map.get()
